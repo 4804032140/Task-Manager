f4fc478d34f92fd49e21682934e147aace9a1e36
restabilize <restabilize@gmail.com>
2023-10-09 20:13:22 -0400

    prepping to code

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//code to connect mongodb, url needs to be edited to our database
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0-4pisv.mongodb.net/COP4331?retryWrites=true&w=majority';
+
+const client = new MongoClient(url);
+client.connect();
+
+
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.listen(5000); // start Node + Express server on port 5000
